58d462df4036319a391e080381a27968e0590346
Harry <manavtiwari1407@gmail.com>
2024-06-05 04:18:50 +0530

    uptill table creation

---
+++ apps/frontend/src/components/ChessBoard.tsx
@@ -0,0 +1,72 @@
+import { Chess, Color, Move, PieceSymbol, Square } from "chess.js";
+import { useState } from "react";
+import { IMove, MOVE } from "../screens/Game";
+import MoveTable from "./Movetable";
+
+
+export const ChessBoard = ({setMoves, chess, board, socket, setBoard,moves }: {
+    setMoves: React.Dispatch<React.SetStateAction<IMove[]>>
+    moves:IMove[],
+    chess: Chess;
+    setBoard: React.Dispatch<React.SetStateAction<({
+        square: Square;
+        type: PieceSymbol;
+        color: Color;
+    } | null)[][]>>;
+    board: ({
+        square: Square;
+        type: PieceSymbol;
+        color: Color;
+    } | null)[][];
+    socket: WebSocket;
+}) => {
+    const [from, setFrom] = useState<null | Square>(null);
+
+    return(
+        <div className="flex">
+            <div className="text-white-200 mr-10">
+                {board.map((row, i) => {
+                    return <div key={i} className="flex">
+                        {row.map((square, j) => {
+                            const squareRepresentation = String.fromCharCode(97 + (j % 8)) + "" + (8 - i) as Square;
+
+                            return <div onClick={() => {
+                                if (!from) {
+                                    setFrom(squareRepresentation);
+                                } else {
+                                    socket.send(JSON.stringify({
+                                        type: MOVE,
+                                        payload: {
+                                            move: {
+                                                from,
+                                                to: squareRepresentation
+                                            }
+                                        }
+                                    }))
+                                    
+                                    setFrom(null)
+                                    chess.move({
+                                        from,
+                                        to: squareRepresentation
+                                    });
+                                    setBoard(chess.board());
+                                    console.log({
+                                        from,
+                                        to: squareRepresentation
+                                    })
+                                    setMoves(moves=>[...moves,{from, to:squareRepresentation}])
+                                }
+                            }} key={j} className={`w-16 h-16 ${(i+j)%2 === 0 ? 'bg-green-500' : 'bg-slate-500'}`}>
+                                <div className="w-full justify-center flex h-full">
+                                    <div className="h-full justify-center flex flex-col">
+                                        {square ? <img className="w-4" src={`/${square?.color === "b" ? square?.type : `${square?.type?.toUpperCase()} copy`}.png`} /> : null} 
+                                    </div>
+                                </div>
+                            </div>
+                        })} 
+                    </div>
+                })}
+        </div>
+    </div>
+    )
+}
---
+++ apps/frontend/src/screens/Game.tsx
@@ -0,0 +1,114 @@
+/* eslint-disable no-case-declarations */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from "react";
+import { Button } from "../components/Button"
+import { ChessBoard } from "../components/ChessBoard"
+import { useSocket } from "../hooks/useSocket";
+import { Chess, Square } from 'chess.js'
+import { useNavigate, useParams } from "react-router-dom";
+import MoveTable from "../components/Movetable";
+
+// TODO: Move together, there's code repetition here
+export const INIT_GAME = "init_game";
+export const MOVE = "move";
+export const GAME_OVER = "game_over";
+export const OPPONENT_DISCONNECTED="opponent_disconnected";
+
+
+interface Metadata{
+    blackPlayer:string,
+    whitePlayer:string,
+    // fen:
+}
+
+export interface IMove{
+    from:Square,
+    to:Square
+}
+  
+
+export const Game = () => {
+    const socket = useSocket();
+    const { gameId } = useParams();
+
+    const navigate = useNavigate();
+    // Todo move to store/context
+    const [chess, _setChess] = useState(new Chess());
+    const [board, setBoard] = useState(chess.board());
+    const [started, setStarted] = useState(false)
+    const [gameMetadata,setGameMetadata]=useState<null|Metadata>(null)
+    const [completed,setCompleted]=useState<Boolean>(false);
+    const [result,setResult]=useState<"WHITE_WINS"|"BLACK_WINS"|"DRAW"| typeof OPPONENT_DISCONNECTED |null>(null);
+    const [moves,setMoves]=useState<IMove[]>([])
+    useEffect(() => {
+        if (!socket) {
+            return;
+        }
+        socket.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+
+            switch (message.type) {
+                case INIT_GAME:
+                    setBoard(chess.board());
+                    setStarted(true)
+                    navigate(`/game/${message.payload.gameId}`)
+                    setGameMetadata({
+                        blackPlayer:message.payload.blackPlayer,
+                        whitePlayer:message.payload.whitePlayer
+                      })
+                    break;
+                case MOVE:
+                    const move = message.payload;
+                    chess.move(move);
+                    setBoard(chess.board());
+                    setMoves(moves=>[...moves,move])
+                    break;
+                case GAME_OVER:
+                    setCompleted(true);
+                    setResult(message.payload.result)
+                    break;
+                case OPPONENT_DISCONNECTED:
+                    setCompleted(true);
+                    setResult(OPPONENT_DISCONNECTED);
+                    break;
+            }
+        }
+    }, [chess, socket]);
+
+    if (!socket) return <div>Connecting...</div>
+
+    return <div className="justify-center flex">
+        <div className="text-white justify-center flex">
+            {gameMetadata?.blackPlayer} vs {gameMetadata?.whitePlayer}
+        </div>
+        {
+            result&&<div className="justify-center flex pt-4 text-white">
+                {result}
+            </div>
+        }
+        <div>
+
+            <div className="pt-8 max-w-screen-lg w-full">
+                <div className="grid grid-cols-6 gap-4 w-full">
+                    <div className="col-span-4 w-full flex justify-center">
+                        <ChessBoard setMoves={setMoves} moves={moves} chess={chess} setBoard={setBoard} socket={socket} board={board} />
+                    </div>
+                    <div className="col-span-2 bg-slate-900 w-full flex justify-center">
+                        <div className="pt-8">
+                            {!started && gameId === "random" && <Button onClick={() => {
+                                socket.send(JSON.stringify({
+                                    type: INIT_GAME
+                                }))
+                            }} >
+                                Play
+                            </Button>}
+                        </div>
+                        <div className="mr-10">            
+                            {moves.length > 0 && <div className="mt-4"><MoveTable moves={moves} /></div>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>                     
+    </div>
+}
